68f2299cad981dff383cb8effbc54f4fef724690
xiaomi <xiaomi@Athira>
2024-10-17 13:26:04 +0530

    Sample

---
+++ FirstMavenProject/src/main/java/accessmodifier/Accessmodifier_example1.java
@@ -0,0 +1,15 @@
+package accessmodifier;
+
+public class Accessmodifier_example1 {
+
+	public static void main(String[] args)
+	{
+		Accessmodifier_example1 obj=new Accessmodifier_example1 ();
+		obj.display();
+	}
+	public void display()
+	{
+		System.out.println("Hello");
+	}
+
+}
