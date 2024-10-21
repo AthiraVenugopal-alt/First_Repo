f05bc34eb209cf2976cd0f1b37244138758b8366
xiaomi <xiaomi@Athira>
2024-10-21 22:56:45 +0530

    Excel sample commited

---
+++ FirstMavenProject/src/main/java/excel/Excel2.java
@@ -0,0 +1,14 @@
+package excel;
+
+import java.io.IOException;
+
+public class Excel2 {
+
+	public static void main(String[] args) throws IOException 
+	{
+		System.out.println(excel_class.getStringdata(1, 0));
+		System.out.println(excel_class.getIntegerdata(1, 1));
+
+	}
+
+}
